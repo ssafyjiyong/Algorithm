98167f8165a268bdb8fa3a2c3119262bb39a7820
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2023-10-18 21:21:04 +0900

    [Silver IV] Title: 스택, Time: 208 ms, Memory: 116188 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+N = int(input())
+q = deque()
+lst = []
+for _ in range(N):
+    lst.append(tuple(map(str, input().split())))
+
+for i in lst:
+    if i[0] == 'push':
+        q.append(i[1])
+    elif i[0] == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.pop())
+    elif i[0] == 'size':
+        print(len(q))
+    elif i[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif i[0] == 'top':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
