f52175e0ecd56a27a5eb2310320988d8ac42d7a4
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2023-10-18 21:12:45 +0900

    [Silver IV] Title: 덱, Time: 212 ms, Memory: 116892 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+N = int(input())
+q = deque()
+lst = []
+for _ in range(N):
+    lst.append(tuple(map(str, input().split())))
+
+for i in lst:
+    if i[0] == 'push_front':
+        q.appendleft(i[1])
+    elif i[0] == 'push_back':
+        q.append(i[1])
+    elif i[0] == 'pop_front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif i[0] == 'pop_back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.pop())
+    elif i[0] == 'size':
+        print(len(q))
+    elif i[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif i[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif i[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
