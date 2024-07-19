63d2a49529ab56adc28f42b0d377df38edc54324
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2024-07-19 21:32:07 +0900

    [level 1] Title: 모의고사, Time: 37.50 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,37 @@
+function solution(answers) {
+    var answer = [];
+    
+    const first = [1,2,3,4,5];
+    const second = [2,1,2,3,2,4,2,5];
+    const third = [3,3,1,1,2,2,4,4,5,5];
+    
+    for (let i = 0; i < answers.length; i++) {
+        first.push(first[i % answers.length])
+        second.push(second[i % answers.length])
+        third.push(third[i % answers.length])
+    };
+    
+    let a = 0
+    let b = 0
+    let c = 0
+    
+    answers.forEach((ans, idx) => {
+        if (ans === first[idx]) {
+            a++
+        }; 
+        if (ans === second[idx]) {
+            b++
+        };
+        if (ans === third[idx]) {
+            c++
+        };
+    });
+    
+    let max = Math.max(a, b, c)
+    
+    if (a === max) answer.push(1)
+    if (b === max) answer.push(2)
+    if (c === max) answer.push(3)
+    
+    return answer;
+}
