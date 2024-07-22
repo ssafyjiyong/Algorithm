3cef1dcf37e9dd02da25c97efc8d49632ba8f184
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2024-07-22 20:50:34 +0900

    [level 1] Title: 실패율, Time: 5595.44 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,12 +1,16 @@
 function solution(N, stages) {
-    var answer = [];
-    for (let i=1; i<=N; i++) {
+    var answer = []
+    
+    for (let i=1; i<=N; i++) {  
         const tried = stages.filter(x => x >= i).length
-        const failed = stages.filter(x => x == i).length
+        const failed = stages.filter(x => x === i).length
+        
         answer.push([failed/tried, i])
-    }
-    answer.sort((a,b) => {
+        }
+    
+    answer.sort((a, b)=>{
         return b[0] - a[0]
     })
+    
     return answer.map(x => x[1]);
 }
