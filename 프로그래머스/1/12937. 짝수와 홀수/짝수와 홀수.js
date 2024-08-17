b478ff5931da6a6872f4983955b716969ff2808e
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2024-08-18 01:39:17 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    var answer = '';
+    if (num % 2 !== 0) {
+        answer = "Odd";
+    } else {
+        answer = "Even";
+    }
+    return answer;
+}
+function solution(num) {
+    var answer = '';
+    if (num % 2 !== 0) {
+        answer = "Odd";
+    } else {
+        answer = "Even";
+    }
+    return answer;
+}
