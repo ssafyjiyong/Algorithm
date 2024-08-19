3a0d6ac2bb2c225d0a4a40cdb2cfa5a3d7733a9a
Jiyong Kim <139519129+ssafyjiyong@users.noreply.github.com>
2024-08-19 20:02:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.93 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,15 @@
 function solution(n) {
+    var answer = 0;
+    
+    let current;
     let prev1 = 1;
     let prev2 = 0;
     
-    for (let i = 2; i <= n; i++) {
-        const current = (prev1 + prev2) % 1234567;
+    for (let i=2; i<=n; i++) {
+        current = (prev1 + prev2) % 1234567;
         prev2 = prev1;
         prev1 = current;
     }
     
-    return prev1;
+    return current;
 }
